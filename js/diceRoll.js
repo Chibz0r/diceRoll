4127088fa6e7855478a729a678b14fefcab006b6
Nathan <nathan@eternitydigital.com>
2018-10-15 15:11:52 +0100

    Multiple dice values
    Added support for multiple dice values and simplified the js to take a variable depending on the button clicked.

--- js/diceRoll.js
+++ js/diceRoll.js
@@ -1,8 +1,9 @@
-var d20
+var dice
+var diceValue
 var result = document.getElementById('result')
-var button = document.querySelector('button')
 
-button.onclick = function() {
-	var d20 = Math.floor(Math.random () * 20) + 1;
-	result.textContent = 'You rolled a ' + d20
+function diceChoice(value){
+	diceValue = value;
+	dice = Math.floor(Math.random () * diceValue) + 1;
+	result.textContent = 'You rolled a ' + dice;
 }
