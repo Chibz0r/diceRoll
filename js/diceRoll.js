c403c3b523503b219ecc43e9aac2c5e10e0355d9
Nathan <nathan@eternitydigital.com>
2018-10-15 15:19:57 +0100

    Text Changes

--- js/diceRoll.js
+++ js/diceRoll.js
@@ -5,5 +5,5 @@ var result = document.getElementById('result')
 function diceChoice(value){
 	diceValue = value;
 	dice = Math.floor(Math.random () * diceValue) + 1;
-	result.textContent = 'You rolled a ' + dice;
+	result.textContent = 'Rolling D' + diceValue + '... you rolled a ' + dice;
 }
