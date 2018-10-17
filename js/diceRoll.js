e72e83f75194d54535855c368a523a495d352461
Nathan <nathan@eternitydigital.com>
2018-10-17 14:27:41 +0100

    Added Event Listener
    Removed inline onClick events from HTML, replacing with value""
    
    Added an Event Listener for "button" clicks
    
    Collect value from button to use as 'diceValue' when calculating random number.

--- js/diceRoll.js
+++ js/diceRoll.js
@@ -1,9 +1,16 @@
 var dice
-var diceValue
-var result = document.getElementById('result')
+var result = document.getElementById('result');
 
-function diceChoice(value){
-	diceValue = value;
+// Roll dice and return random number
+function diceRoll() {
+	var diceValue = this.value;
 	dice = Math.floor(Math.random () * diceValue) + 1;
-	result.textContent = 'Rolling D' + diceValue + '... you rolled a ' + dice;
+	result.textContent = 'Rolling D' + diceValue + ' ... you rolled a ' + dice;
+}
+
+// Listen for when a button is clicked and trigger dice roll
+var button = document.querySelectorAll('button');
+
+for (var i=0; i < button.length ; i++) {
+	button[i].addEventListener('click', diceRoll);
 }
